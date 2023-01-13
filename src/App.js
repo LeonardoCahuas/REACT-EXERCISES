b4ce0744cf0ef4d34479c5816183051a2aab6771
LeonardoCahuas <leocahuas99@gmail.com>
2023-01-13 22:57:09 +0100

    props1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Hello } from "./hello";
+import { Welcome } from "./welcome";
 export class App extends React.Component{
     render(){
         return(
             <div>
-              <Hello/>
+              <Welcome name="luca"/>
             </div>
         );
     }
