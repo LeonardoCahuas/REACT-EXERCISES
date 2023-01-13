211a36b16d7361ded39054082a251cba1e4e6040
LeonardoCahuas <leocahuas99@gmail.com>
2023-01-13 22:17:36 +0100

    exercise-8

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
-
-function App() {
-  return (
-    <h1>
-      Hello, World!
-    </h1>
-  );
+import React from "react";
+import { Hello } from "./hello";
+export class App extends React.Component{
+    render(){
+        return(
+            <div>
+              <Hello/>
+            </div>
+        );
+    }
 }
 
 export default App;
+
+
+
