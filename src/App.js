83bbccd10d61c9fca266c2180fedc22f9aee9e8b
LeonardoCahuas <leocahuas99@gmail.com>
2023-01-13 19:11:02 +0100

    exercise-4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import './App.css';
+import Sayname from './sayName';
 
-function App() {
-  return (
-    <h1>
-      Hello, World!
-    </h1>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>
+        Hello, World!
+      </h1>
+      <Sayname name="<h3> prova... </h3>"/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/sayName.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Sayname extends React.Component{
+    render(){
+        return(
+            <span>{this.props.name} !</span>
+        );
+    }
+}
+export default Sayname;
