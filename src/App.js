ac30b8264e73050cf588e4d5b8f65d807740e59e
LeonardoCahuas <leocahuas99@gmail.com>
2023-01-17 14:09:04 +0100

    props-4

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ export class App extends React.Component{
     render(){
         return(
             <div>
-              <Welcome name="alex" age="32"/>
+              <Welcome name={<strong>Alex</strong>} age="32"/>
             </div>
         );
     }
 }
-
 export default App;
+
+/* Passing a JSX expression in the name prop makes it render itself 
+as the respective HTML tag and is considered valid for JSX
+In order to place a JSX expression in a prop it must be passed into curly brackets */
 
 
 
