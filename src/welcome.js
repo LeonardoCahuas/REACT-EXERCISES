244de9e054f40e933602dceafd4042bfeb4608bf
LeonardoCahuas <leocahuas99@gmail.com>
2023-01-22 18:14:38 +0100

    conditional-3

--- src/welcome.js
+++ src/welcome.js
@@ -5,7 +5,7 @@ export class Welcome extends React.Component{
         return(
             <div>
                 <p>Welcome, {this.props.name ? this.props.name  : "lucio"}!</p>
-                <Age age={this.props.age>18 ? this.props.age : "not valid" }/>
+                {this.props.age>18 && this.props.age<55 && <Age age={this.props.age}/>}
             </div>
         );
     }
