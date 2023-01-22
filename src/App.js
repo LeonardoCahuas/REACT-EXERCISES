10372a1f1d852683a40cc5fd8d5b4a870a8e5f8b
LeonardoCahuas <leocahuas99@gmail.com>
2023-01-22 18:24:11 +0100

    conditional-5

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export class App extends React.Component{
     render(){
         return(
             <div>
-              <Welcome name="Jhon" age={34}/>
+              <Welcome name="Jhon" age={13}/>
             </div>
         );
     }
--- src/age.js
+++ src/age.js
@@ -3,7 +3,11 @@ import React from "react";
 export class Age extends React.Component{
     render(){
         return(            
-            <p>Your age is: {this.props.age ? this.props.age : "unknown"}</p>           
+            <div>
+                {this.props.age > 18
+                  ? <p>Your age is: {this.props.age ? this.props.age : "unknown"}</p>
+                  : <p>You are very young!</p>}
+            </div>
         );
     }
 }
