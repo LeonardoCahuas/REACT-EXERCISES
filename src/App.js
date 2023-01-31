edec2ee51e0bd91052e8fc5aa5f7a6ed272e16ce
LeonardoCahuas <leocahuas99@gmail.com>
2023-01-31 12:59:11 +0100

    events-3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { ClickCounter } from "./ClickCounter";
+import { ClickTracker } from "./ClickTracker";
 export class App extends React.Component{
     render(){
         return(
             <div>
-              <ClickCounter/>
+              <ClickTracker/>
             </div>
         );
     }
