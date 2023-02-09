5f61d8b3738f9f7b9fa14be0c2c60fd464eb6535
LeonardoCahuas <leocahuas99@gmail.com>
2023-02-09 23:43:19 +0100

    function component 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './index.css'
-import Welcome from './Welcome';
+import Sum from './Sum';
  
 export class App extends React.Component{
     
     render(){
         return(
             <div>
-                <Welcome/>
+                <Sum numbers={[1, 5, 7]}/>
             </div>
         ); 
     }
