4dda287c726cb961f8c7153567529878a611831b
LeonardoCahuas <leocahuas99@gmail.com>
2023-02-12 17:00:32 +0100

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
+
 import React from 'react'
-import FilteredList from './FilteredList';
+
+import DisplayCounter from './DisplayCounter';
 
 export class App extends React.Component{
-    
+
     render(){
         return(
             <div>
-               <FilteredList li={[
-                   {name:"jhon", age: 30, id:6},
-                   {name: "lia", age: 17, id:3},
-                   {name: "tom", age: 15, id:9},
-                   {name: "sara", age: 47, id:1}
-                ]}/>
+               
+               <DisplayCounter initialValue={10}/>
             </div>
         ); 
-    }
-}
-export default App;
+    }}
 
-/* Passing a JSX expression in the name prop makes it render itself 
-as the respective HTML tag and is considered valid for 
-In order to place a JSX expression in a prop it must be passed into curly brackets */
-
-
-
+export default App
